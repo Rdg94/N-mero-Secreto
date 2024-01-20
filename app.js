777916e3dd4a537fc40852240b25c964bd16d145
Rdg94 <rodrigo_paulino_11@hotmail.com>
2024-01-20 10:24:28 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Bem vindo ao jogo do número secreto!");
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+  chute = prompt("Digite um número de 1 a 100.");
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}.`);
+    } else {
+      alert(`O número secrto é maior que ${chute}.`);
+    }
+    tentativas++;
+  }
+}
+
+let palavraTentaviva = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Parabéns, você acertou o número secreto ${numeroSecreto} depois de ${tentativas} ${palavraTentaviva}.`
+);
